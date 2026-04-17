5f541d515633d814586cd6322ebe02ef57c4d970
Agila-learning <agilaagi0922@gmail.com>
2026-04-17 11:14:42 +0530

    fix: import missing useNavigate from react-router-dom in Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import {
